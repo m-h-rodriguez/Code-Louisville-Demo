55dcd56fd0c536b31edac078517883d7077d99be
m-h-rodriguez <105326343+m-h-rodriguez@users.noreply.github.com>
2022-05-27 13:34:02 -0400

    test

---
+++ slackchallenge_names.py
@@ -0,0 +1,12 @@
+
+names_dict= {
+    "tre": "robinson", 
+    "justin": "schuhmann", 
+    "richard": "oden", 
+    "micah": "potts", 
+    "clay": "morton", 
+    "anne": "ensign", 
+    "jeff": "dixon"}
+
+for first, last in names_dict.items():
+    print(first[0].title()+".", last.title())
